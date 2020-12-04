08a193817976438f35749ee0d26dd906386c5de6
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-12-03 19:13:36 -0600

    added welcome router

--- index.js
+++ index.js
@@ -7,14 +7,10 @@ const server = express()
 const port = 4000
 
 server.use(express.json())
+
 server.use(usersRouter)
+server.use(welcomeRouter)
 
-
-
-
-
-// create endpoint that returns all the posts for a user
-// create endpoint for adding a new post for a user
 
 server.listen(port, () => {
 	console.log(`Server running at http://localhost:${port}`)
